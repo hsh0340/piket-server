8c597ed85b5e322f5d1ba64275ac0de9e5924f23
hsh0340 <hsh0340@naver.com>
2023-09-19 18:31:24 +0900

    Feat: 기본 모듈들 추가, 폴더링

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { UserModule } from './modules/user/user.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PrismaModule, UserModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
