7055589227bb3982dbb54d5f250450454f28b591
hsh0340 <hsh0340@naver.com>
2023-09-20 14:06:07 +0900

    Feat: Config Module을 app module에 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { HealthModule } from './modules/health/health.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
