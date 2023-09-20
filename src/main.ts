12e54830b969d1377747844feda8a71a8756ad00
hsh0340 <hsh0340@naver.com>
2023-09-20 14:11:48 +0900

    Refactor: .env를 사용하지 않고 config service 이용한 코드로 수정

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.enableCors();
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000);
+  const port = configService.get('PORT');
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
