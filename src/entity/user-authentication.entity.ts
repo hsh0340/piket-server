3af60a0c8ec4bd9ccd64f5ec2f34560f8b30d0a2
hsh0340 <hsh0340@naver.com>
2023-09-21 16:00:20 +0900

    Feat: user auth entity 생성

---
+++ src/entity/user-authentication.entity.ts
@@ -0,0 +1,18 @@
+import { UserAuthentication } from '@prisma/client';
+
+export class UserAuthenticationEntity implements UserAuthentication {
+  id: number;
+  userNo: number;
+  password: string;
+  name: string;
+  cellPhone: string;
+  sex: number;
+  tosAgree: number;
+  personalInfoAgree: number;
+  ageLimitAgree: number;
+  mailAgree: number;
+  notificationAgree: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
