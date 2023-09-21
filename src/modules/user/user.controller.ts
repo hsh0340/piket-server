984aecf452dacc0a13d2b9b18bb836b6d109b87c
hsh0340 <hsh0340@naver.com>
2023-09-21 14:57:58 +0900

    Docs: 구현해야 할 API 주석 작성

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { UserService } from '@src/modules/user/user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  // 회원가입 API
+  @Post()
+  join() {}
+
+  // 이메일로그인 API
+  @Post('login')
+  login() {}
+
+  // 카카오로그인 API
+
+  // 네이버로그인 API
+
+  // 구글로그인 API
+
+  // 프로필조회 API
+
+  // 프로필수정 API
 }
