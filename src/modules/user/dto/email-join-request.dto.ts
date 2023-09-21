8cb6d1d6e970a9bc471642b11fc7b69ba054a281
hsh0340 <hsh0340@naver.com>
2023-09-21 16:22:24 +0900

    Feat: email join request dto 작성

---
+++ src/modules/user/dto/email-join-request.dto.ts
@@ -0,0 +1,12 @@
+export class EmailJoinRequestDto {
+  cellPhone: string;
+  password: string;
+  email: string;
+  name: string;
+  sex: number;
+  tosAgree: number;
+  personalInfoAgree: number;
+  ageLimitAgree: number;
+  mailAgree: number;
+  notificationAgree: number;
+}
