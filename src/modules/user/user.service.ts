f744ea01daeffd8577d9c385ab3377e4f661bd45
hsh0340 <hsh0340@naver.com>
2023-09-22 16:08:19 +0900

    Feat: try catch 문 추가

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { EmailJoinRequestDto } from '@src/modules/user/dto/email-join-request.dto';
 import { PrismaService } from '@src/modules/prisma/prisma.service';
 
@@ -20,30 +20,36 @@ export class UserService {
       notificationAgree,
     } = emailJoinRequestDto;
 
-    return this.prismaService.$transaction(async (tx) => {
-      const user = await tx.user.create({
-        data: {
-          email,
-          loginType: 0,
-          role: 0,
-        },
-      });
+    try {
+      this.prismaService.$transaction(async (tx) => {
+        const user = await tx.user.create({
+          data: {
+            email,
+            loginType: 0,
+            role: 0,
+          },
+        });
 
-      const userAuth = await tx.userAuthentication.create({
-        data: {
-          userNo: user.no,
-          password,
-          name,
-          cellPhone,
-          sex,
-          tosAgree,
-          personalInfoAgree,
-          ageLimitAgree,
-          mailAgree,
-          notificationAgree,
-        },
+        const userAuth = await tx.userAuthentication.create({
+          data: {
+            userNo: user.no,
+            password,
+            name,
+            cellPhone,
+            sex,
+            tosAgree,
+            personalInfoAgree,
+            ageLimitAgree,
+            mailAgree,
+            notificationAgree,
+          },
+        });
+        return { isSuccess: true, code: 200, user, userAuth };
       });
-      return { isSuccess: true, code: 200, user, userAuth };
-    });
+    } catch (err) {
+      console.log(err);
+
+      throw new InternalServerErrorException(err);
+    }
   }
 }
