e8e602691e2dd375d28aebbdee2027f4b0ca6938
hsh0340 <hsh0340@naver.com>
2023-09-22 10:31:47 +0900

    Feat: DTO validation 코드 추가

--- src/modules/user/dto/email-join-request.dto.ts
+++ src/modules/user/dto/email-join-request.dto.ts
@@ -1,12 +1,38 @@
+import { IsEnum, IsNotEmpty, IsString, Matches } from "class-validator";
+
 export class EmailJoinRequestDto {
+  @IsNotEmpty()
+  @IsString()
   cellPhone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
   sex: number;
+
+  @IsNotEmpty()
   tosAgree: number;
+
+  @IsNotEmpty()
   personalInfoAgree: number;
+
+  @IsNotEmpty()
   ageLimitAgree: number;
+
+  @IsNotEmpty()
   mailAgree: number;
+
+  @IsNotEmpty()
   notificationAgree: number;
 }
