71b5c92f417971f03aaf64e5ca631a85e15fd2fa
hsh0340 <hsh0340@naver.com>
2023-09-25 17:44:02 +0900

    Feat: response 타입 구현

--- src/common/dto/response.dto.ts
+++ src/common/dto/response.dto.ts
@@ -1,5 +1,12 @@
-export class ResponseDto<T> {
-  isSuccess: boolean;
+export class SuccessResponseDto<T> {
+  isSuccess: true;
+  code: 200;
+  message: string;
+  result: T;
+}
+
+export class FailureResponseDto<T> {
+  isSuccess: false;
   code: number;
   message: string;
   result: T;
