ff6332e43dd54eb419b680a5a7eaf4a70007381b
hsh0340 <hsh0340@naver.com>
2023-09-25 20:09:04 +0900

    Feat: InvalidRequestException 구현

---
+++ src/common/exceptions/invalid.exception.ts
@@ -0,0 +1,9 @@
+import { BaseException } from '@src/common/exceptions/base.exception';
+import { RequestExceptionCodeEnum } from '@src/common/enums/exception.enum';
+import { HttpStatus } from '@nestjs/common';
+
+export class InvalidRequestException extends BaseException {
+  constructor() {
+    super(RequestExceptionCodeEnum.InvalidRequest, HttpStatus.BAD_REQUEST);
+  }
+}
