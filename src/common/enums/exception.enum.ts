87483c468ca1e3197a9d9632d7ef8e7413bad29c
hsh0340 <hsh0340@naver.com>
2023-09-26 14:57:24 +0900

    Feat: 상태코드 enum 생성

--- src/common/enums/exception.enum.ts
+++ src/common/enums/exception.enum.ts
@@ -1,6 +1,15 @@
 export enum RequestExceptionCodeEnum {
   // 유효성 검사에 실패하였습니다.
   InvalidRequest = '2000',
+
+  // 이미 존재하는 전화번호입니다.
+  PhoneExist = '3000',
+  // 이미 존재하는 이메일입니다.
+  EmailExist = '3001',
+  // 이 이메일로 가입된 사용자가 없습니다.
+  UserNotFound = '3002',
+  // 비밀번호가 일치하지 않습니다.
+  PasswordMismatch = '3003',
 }
 
 export enum UncatchedExceptionCodeEnum {
