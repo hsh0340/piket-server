c86b29a2585ea3cdd52a3b5ce54711d443176e02
hsh0340 <hsh0340@naver.com>
2023-09-27 17:05:44 +0900

    Chore: mailer module 의존성 설치

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -6,19 +6,42 @@ import { AuthModule } from '@src/modules/auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigService } from '@nestjs/config';
 import * as redisStore from 'cache-manager-ioredis';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     AuthModule,
     PrismaModule,
-    CacheModule.registerAsync({
-      useFactory: (configService: ConfigService) => ({
-        store: redisStore,
-        host: configService.get<string>('CACHE_HOST'),
-        port: configService.get<number>('CACHE_PORT'),
-        ttl: 100000,
+    CacheModule.register({
+      store: redisStore,
+      host: '127.0.0.1',
+      port: 6379,
+      ttl: 100000, // 없는 경우 default 5초
+    }),
+    // CacheModule.registerAsync({
+    //   useFactory: (configService: ConfigService) => ({
+    //     store: redisStore,
+    //     host: configService.get<string>('CACHE_HOST'),
+    //     port: configService.get<number>('CACHE_PORT'),
+    //     ttl: 100000,
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: 'smtp.example.com',
+          port: 587,
+          auth: {
+            user: 'email address',
+            pass: 'password',
+          },
+        },
+        defaults: {
+          from: '"no-reply" <email address>',
+        },
+        preview: true,
       }),
-      inject: [ConfigService],
     }),
   ],
   providers: [UserService],
