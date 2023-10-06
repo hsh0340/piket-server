2d960f2a8e62ccc63d057461377f9f4a01781092
hsh0340 <hsh0340@naver.com>
2023-10-06 16:57:53 +0900

    Feat: brand 컨트롤러 api 엔드포인트 생성

--- src/modules/brand/brand.controller.ts
+++ src/modules/brand/brand.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('brand')
-export class BrandController {}
+export class BrandController {
+  // 신규 브랜드 등록 API
+  @Post()
+  createBrand() {}
+
+  // 브랜드 수정 API
+  @Patch()
+  updateBrand() {}
+
+  // 브랜드 목록 API
+  @Get()
+  getAllBrands() {}
+
+  // 브랜드 삭제 API
+  @Delete()
+  deleteBrand() {}
+}
