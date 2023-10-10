f9fe2c0c245ad6b3d1cdefd49545d43b20bd7203
hsh0340 <hsh0340@naver.com>
2023-10-10 16:04:00 +0900

    Feat: 브랜드 카테고리 enum 생성, 대문자 변경

--- src/common/constants/enum.ts
+++ src/common/constants/enum.ts
@@ -1,16 +1,43 @@
 export enum Agreement {
-  reject = 0,
-  accept = 1,
+  REJECT = 0,
+  ACCEPT = 1,
 }
 
 export enum Sex {
-  male = 0,
-  female = 1,
+  MALE = 0,
+  FEMALE = 1,
 }
 
 export enum LoginType {
-  email = 0,
-  kakao = 1,
-  google = 2,
-  naver = 3,
+  EMAIL = 0,
+  KAKAO = 1,
+  GOOGLE = 2,
+  NAVER = 3,
+}
+
+export enum BrandCategory {
+  // 코스메틱 : 0
+  COSMETIC = 0,
+  // 육아 : 1
+  PARENTING = 1,
+  // 맛집 : 2
+  RESTAURANT = 2,
+  // 패션 : 3
+  FASHION = 3,
+  // 가전 : 4
+  HOME_APPLIANCE = 4,
+  // 여행 : 5
+  TRAVEL = 5,
+  // 반려 : 6
+  PET = 6,
+  // 식품 : 7
+  FOOD = 7,
+  // 스포츠 : 8
+  SPORTS = 8,
+  // 문화 : 9
+  CULTURE = 9,
+  // 생활 : 10
+  LIVING = 10,
+  // 기타 : 11
+  ETC = 11,
 }
