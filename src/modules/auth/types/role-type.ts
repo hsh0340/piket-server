cfc749ad3f9d729b9a4c4f91bd181ae5d9559159
hsh0340 <hsh0340@naver.com>
2023-10-10 10:11:07 +0900

    Feat: 유저 권한 enum 생성

---
+++ src/modules/auth/types/role-type.ts
@@ -0,0 +1,12 @@
+export enum RoleType {
+  // 가입만 한 상태(피케터 / 광고주 선택하지 않은 상태) : 0
+  UNDEFINED = 0,
+  // 피케터 : 1
+  PIKETER = 1,
+  // 광고주 : 2
+  ADVERTISER = 2,
+  // 회원 아닌 상태 : 3
+  ANONYMOUS = 3,
+  // 어드민 : 4
+  ADMIN = 4,
+}
