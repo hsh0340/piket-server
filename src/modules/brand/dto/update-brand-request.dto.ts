289c4e5dd0302a6ca77ccaa08d8bcd0a277656f7
hsh0340 <hsh0340@naver.com>
2023-10-11 16:35:41 +0900

    Fix: brand description validation 수정

--- src/modules/brand/dto/update-brand-request.dto.ts
+++ src/modules/brand/dto/update-brand-request.dto.ts
@@ -1,7 +1,7 @@
 import {
   IsEnum,
   IsNotEmpty,
-  IsOptional,
+  IsString,
   Matches,
   MaxLength,
 } from 'class-validator';
@@ -15,7 +15,7 @@ export class UpdateBrandRequestDto {
   /**
    * @member categoryId [카테고리 고유 Id] enum 타입 (12개 카테고리 중 하나), 필수 입력
    * @member name [브랜드 네임] 1자 이상 10자 이하, 한글/영문/숫자/공백 기입 가능, 엔터 및 특수문자 불가능
-   * @member description [브랜드 한 줄 설명] 필수 값 아님, 30자 이하, 한글/영문/숫자/공백 기입 가능, 엔터 및 특수문자 불가능
+   * @member description [브랜드 한 줄 설명] 필수 값 이지만 빈 문자열 가능, 30자 이하, 한글/영문/숫자/공백 기입 가능, 엔터 및 특수문자 불가능
    */
   @IsNotEmpty()
   @IsEnum(BrandCategory)
@@ -26,7 +26,7 @@ export class UpdateBrandRequestDto {
   @MaxLength(10)
   name: string;
 
-  @IsOptional()
+  @IsString()
   @Matches(/^[a-zA-Z0-9가-힣 ]*$/)
   @MaxLength(30)
   description: string;
