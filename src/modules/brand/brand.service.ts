440a6891b04a519b5a837e7b4f9b07ebda1c57f2
hsh0340 <hsh0340@naver.com>
2023-10-11 16:12:39 +0900

    Feat: 브랜드 수정 메서드에서 수정 후 브랜드 정보가 이미 존재하는 브랜드와 일치할 경우 예외처리

--- src/modules/brand/brand.service.ts
+++ src/modules/brand/brand.service.ts
@@ -78,6 +78,7 @@ export class BrandService {
    * @param brandId 수정하려는 브랜드의 고유 번호
    * @param updateBrandRequestDto 브랜드 수정 DTO
    * @return void
+   * @exception 수정 후 브랜드 정보가 이미 존재하는 브랜드와 일치할 경우 BrandExistsException 을 반환합니다.
    * @exception 수정하려는 브랜드가 존재하지 않을 경우 BrandNotExistsException 을 반환합니다.
    * @exception 업데이트 된 브랜드의 정보가 없을 경우 NoBrandInfoUpdatedException 을 반환합니다.
    * @exception 브랜드 업데이트에 실패한 경우 BrandNotUpdatedException 을 반환합니다.
