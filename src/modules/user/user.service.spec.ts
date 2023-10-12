835e1ee2323631d3c02ac09356ccfb045c9b8f7b
hsh0340 <hsh0340@naver.com>
2023-10-12 18:22:52 +0900

    Test: getUserByEmail unit test 완료

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -5,7 +5,9 @@ import { AuthService } from '@src/modules/auth/services/auth.service';
 import { MailerService } from '@nestjs-modules/mailer';
 import { mockPrismaService } from '@test/mock/mock-prisma-service';
 import { mockAuthService, mockMailerService } from '@test/mock/mock-services';
-import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { UserEntity } from '@src/entity/user.entity';
+import { EmailDto } from '@src/modules/user/dto/email.dto';
 
 describe('UserService', () => {
   let userService: UserService;
@@ -42,5 +44,42 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(userService).toBeDefined();
+  });
+
+  describe('getUserByEmail', () => {
+    let user: UserEntity;
+    let emailDto: EmailDto;
+
+    beforeEach(() => {
+      user = new UserEntity();
+      emailDto = new EmailDto();
+    });
+
+    it('유저 정보를 리턴해야합니다.', async () => {
+      /*
+       * findFirst 메서드를 mocking 하고, 이 메서드가 user 객체를 반환하도록 설정
+       */
+      mockPrismaService.user.findFirst.mockResolvedValue(user);
+
+      const result = await userService.getUserByEmail(emailDto); // UserEntity {}
+
+      /*
+       * findFirst 메서드가 올바른 where 조건으로 호출되었는지 확인
+       */
+      expect(mockPrismaService.user.findFirst).toBeCalledWith({
+        where: emailDto,
+      });
+
+      /**
+       * 반환된 결과가 user 객체와 일치하는지 확인
+       */
+      expect(result).toStrictEqual(user);
+    });
+
+    it('유저가 존재하지 않는 경우 null 을 리턴해야합니다.', async () => {
+      mockPrismaService.user.findFirst.mockResolvedValue(null);
+
+      await expect(userService.getUserByEmail(emailDto)).resolves.toBeNull();
+    });
   });
 });
