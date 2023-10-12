309dfde964170f83219f6c08213980316ad10857
hsh0340 <hsh0340@naver.com>
2023-10-12 15:09:27 +0900

    Test: user service 테스트 파일 생성, 주입될 서비스 mocking

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,5 +1,46 @@
 import { UserService } from '@src/modules/user/user.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@src/modules/prisma/prisma.service';
+import { AuthService } from '@src/modules/auth/services/auth.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { mockPrismaService } from '@test/mock/mock-prisma-service';
+import { mockAuthService, mockMailerService } from '@test/mock/mock-services';
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
 
 describe('UserService', () => {
   let userService: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: {},
+        },
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+        {
+          provide: MailerService,
+          useValue: mockMailerService,
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(userService).toBeDefined();
+  });
 });
--- test/mock/mock-services.ts
+++ test/mock/mock-services.ts
@@ -2,3 +2,7 @@ export const mockAuthService = {
   issueAccessToken: jest.fn(),
   issueRefreshToken: jest.fn(),
 };
+
+export const mockMailerService = {
+  sendMail: jest.fn(),
+};
