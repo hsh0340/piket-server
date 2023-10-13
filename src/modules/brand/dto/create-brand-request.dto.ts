36c0a9ca04b521cf8b93d0a9981ea4ebf1aed120
hsh0340 <hsh0340@naver.com>
2023-10-13 17:47:53 +0900

    Comment: create brand request dto 주석 수정

--- src/modules/brand/dto/create-brand-request.dto.ts
+++ src/modules/brand/dto/create-brand-request.dto.ts
@@ -9,14 +9,11 @@ import { BrandCategory } from '@src/common/constants/enum';
 
 /**
  * 브랜드 생성 DTO
- * @author seungha
+ * @property {number} categoryId 카테고리 고유 Id
+ * @property {string} name 브랜드 네임
+ * @property {string} description 브랜드 한 줄 설명
  */
 export class CreateBrandRequestDto {
-  /**
-   * @member categoryId [카테고리 고유 Id] enum 타입 (12개 카테고리 중 하나), 필수 입력
-   * @member name [브랜드 네임] 1자 이상 10자 이하, 한글/영문/숫자/공백 기입 가능, 엔터 및 특수문자 불가능
-   * @member description [브랜드 한 줄 설명] 필수 값 이지만 빈 문자열 가능, 30자 이하, 한글/영문/숫자/공백 기입 가능, 엔터 및 특수문자 불가능
-   */
   @IsNotEmpty()
   @IsEnum(BrandCategory)
   categoryId: number;
