ce63bb93d6228dcfe89bc08aa40e5ef20c2b0010
hsh0340 <hsh0340@naver.com>
2023-10-13 17:49:40 +0900

    Comment: get all brands dto 주석 수정

--- src/modules/brand/dto/get-all-brands.dto.ts
+++ src/modules/brand/dto/get-all-brands.dto.ts
@@ -1,3 +1,10 @@
+/**
+ * 브랜드 목록 조회 DTO
+ * @property {number} id 브랜드 고유번호
+ * @property {number} categoryId 카테고리 고유번호
+ * @property {string} name 브랜드명
+ * @property {string} description 브랜드 설명
+ */
 export class GetAllBrandsDto {
   id: number;
   categoryId: number;
