ac5b66eb6ad683e7bd907e8f07c298de4c96053a
hsh0340 <hsh0340@naver.com>
2023-10-13 11:23:11 +0900

    Feat: brand entity 생성

---
+++ src/entity/brand.entity.ts
@@ -0,0 +1,11 @@
+import { Brand } from '@prisma/client';
+
+export class BrandEntity implements Brand {
+  id: number;
+  advertiserNo: number;
+  categoryId: number;
+  name: string;
+  description: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
