f29e429184d1c746443216e8e8b942bf154e215b
hsh0340 <hsh0340@naver.com>
2023-10-16 14:25:27 +0900

    Feat: campaign controller에 interceptor decorator 추가

--- src/modules/campaign/campaign.controller.ts
+++ src/modules/campaign/campaign.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { CampaignService } from '@src/modules/campaign/campaign.service';
+import { ResponseSerializationInterceptor } from '@src/common/interceptors/response-serialization.interceptor';
 
+@UseInterceptors(ResponseSerializationInterceptor)
 @Controller('campaign')
 export class CampaignController {
   constructor(private readonly campaignService: CampaignService) {}
