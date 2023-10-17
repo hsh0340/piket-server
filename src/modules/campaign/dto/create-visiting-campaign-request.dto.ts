ef7ae1f2a3f9a555a09273b6f02912ba45163895
hsh0340 <hsh0340@naver.com>
2023-10-17 18:21:08 +0900

    Fix: picktype으로 변경

---
+++ src/modules/campaign/dto/create-visiting-campaign-request.dto.ts
@@ -0,0 +1,34 @@
+import { PartialType, PickType } from '@nestjs/swagger';
+import { CreateCampaignRequestDto } from '@src/modules/campaign/dto/create-campaign-request.dto';
+
+export class CreateVisitingCampaignRequestDto extends PickType(
+  CreateCampaignRequestDto,
+  [
+    'brandId',
+    'title',
+    'info',
+    'reward',
+    'channel',
+    'recruitment',
+    'recruitmentCondition',
+    'recruitmentStartsDate',
+    'recruitmentEndsDate',
+    'selectionEndsDate',
+    'submitStartsDate',
+    'submitEndsDate',
+    'postingGuide',
+    'caution',
+    'hashtag',
+    'company',
+    'managerName',
+    'managerTel',
+    'managerEmail',
+    'options',
+  ] as const,
+) {
+  visitingAddr: string;
+  visitingTime: string;
+  note: string;
+  visitingEndsDate: Date;
+  servicePrice: number;
+}
