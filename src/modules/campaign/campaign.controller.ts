895110c53748c9eb83d6ab60027fe4a360d96141
hsh0340 <hsh0340@naver.com>
2023-10-18 19:57:28 +0900

    Feat: campaign controller 방문형 캠페인 등록 엔드포인트 작성

--- src/modules/campaign/campaign.controller.ts
+++ src/modules/campaign/campaign.controller.ts
@@ -1,17 +1,41 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CampaignService } from '@src/modules/campaign/campaign.service';
 import { ResponseSerializationInterceptor } from '@src/common/interceptors/response-serialization.interceptor';
 
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '@src/modules/auth/guards/roles.guard';
+import { Roles } from '@src/modules/auth/decorators/roles.decorator';
+import { RoleType } from '@src/modules/auth/types/role-type';
+import { User } from '@src/modules/auth/decorators/user.decorator';
+import { UserEntity } from '@src/entity/user.entity';
+import { CreateVisitingCampaignRequestDto } from '@src/modules/campaign/dto/create-visiting-campaign-request.dto';
+
 @UseInterceptors(ResponseSerializationInterceptor)
-@Controller('campaign')
+@Controller('campaigns')
 export class CampaignController {
   constructor(private readonly campaignService: CampaignService) {}
 
   /*
-   * 캠페인 등록 API
+   * 방문형 캠페인 등록 API
    */
-  async createCampaign(createCampaignRequestDto: CreateCampaignRequestDto) {
-    await this.campaignService.createCampaign();
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(RoleType.ADVERTISER)
+  @Post('visitings')
+  async createCampaign(
+    @User() advertiser: UserEntity,
+    @Body() createVisitingCampaignRequestDto: CreateVisitingCampaignRequestDto,
+  ) {
+    await this.campaignService.createCampaign(
+      advertiser,
+      createVisitingCampaignRequestDto,
+    );
     return '캠페인 등록에 성공하였습니다.';
   }
 }
