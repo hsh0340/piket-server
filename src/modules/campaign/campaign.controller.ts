2ba632eeb5c33543001961973eb53e05470a4766
hsh0340 <hsh0340@naver.com>
2023-10-24 17:40:11 +0900

    Feat: 배송형 캠페인 등록 API 엔드포인트 생성

--- src/modules/campaign/campaign.controller.ts
+++ src/modules/campaign/campaign.controller.ts
@@ -1,14 +1,12 @@
 import {
   Body,
   Controller,
-  Get,
   Post,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { CampaignService } from '@src/modules/campaign/campaign.service';
 import { ResponseSerializationInterceptor } from '@src/common/interceptors/response-serialization.interceptor';
-
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '@src/modules/auth/guards/roles.guard';
 import { Roles } from '@src/modules/auth/decorators/roles.decorator';
@@ -16,7 +14,8 @@ import { RoleType } from '@src/modules/auth/types/role-type';
 import { User } from '@src/modules/auth/decorators/user.decorator';
 import { UserEntity } from '@src/entity/user.entity';
 import { CreateVisitingCampaignRequestDto } from '@src/modules/campaign/dto/create-visiting-campaign-request.dto';
-import { CreateWritingCampaignRequestDto } from "@src/modules/campaign/dto/create-writing-campaign-request.dto";
+import { CreateWritingCampaignRequestDto } from '@src/modules/campaign/dto/create-writing-campaign-request.dto';
+import { CreateDeliveryCampaignRequestDto } from '@src/modules/campaign/dto/create-delivery-campaign-request.dto';
 
 @UseInterceptors(ResponseSerializationInterceptor)
 @Controller('campaigns')
@@ -32,11 +31,12 @@ export class CampaignController {
   async createVisitingCampaign(
     @User() advertiser: UserEntity,
     @Body() createVisitingCampaignRequestDto: CreateVisitingCampaignRequestDto,
-  ) {
+  ): Promise<string> {
     await this.campaignService.createVisitingCampaign(
       advertiser,
       createVisitingCampaignRequestDto,
     );
+
     return '캠페인이 등록되었습니다.';
   }
 
@@ -49,11 +49,30 @@ export class CampaignController {
   async createWritingCampaign(
     @User() advertiser: UserEntity,
     @Body() createWritingCampaignRequestDto: CreateWritingCampaignRequestDto,
-  ) {
+  ): Promise<string> {
     await this.campaignService.createWritingCampaign(
       advertiser,
-      createWritingCampaignRequestDto
+      createWritingCampaignRequestDto,
     );
+
+    return '캠페인이 등록되었습니다.';
+  }
+
+  /*
+   * 배송형 캠페인 등록 API
+   */
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(RoleType.ADVERTISER)
+  @Post('delivery')
+  async createDeliveryCampaign(
+    @User() advertiser: UserEntity,
+    @Body() createDeliveryCampaignRequestDto: CreateDeliveryCampaignRequestDto,
+  ): Promise<string> {
+    await this.campaignService.createDeliveryCampaign(
+      advertiser,
+      createDeliveryCampaignRequestDto,
+    );
+
     return '캠페인이 등록되었습니다.';
   }
 }
