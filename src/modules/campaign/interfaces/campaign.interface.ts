0c6051591e1125ec029ddd31acc9bc27d7e76b58
hsh0340 <hsh0340@naver.com>
2023-10-24 15:30:41 +0900

    Feat: 캠페인 생성 input 인터페이스 구현

---
+++ src/modules/campaign/interfaces/campaign.interface.ts
@@ -0,0 +1,70 @@
+export interface CreateCommonCampaignInput {
+  brandId: number;
+  advertiserNo: number;
+  title: string;
+  reward: number;
+  channelConditionId: number;
+  postingGuide: string | null;
+  caution: string | null;
+  type: number;
+  recruitment: number;
+  recruitmentStartsDate: Date;
+  recruitmentEndsDate: Date;
+  selectionEndsDate: Date;
+  submitStartsDate: Date;
+  submitEndsDate: Date;
+  hashtag: string;
+  company: string;
+  managerName: string;
+  managerTel: string;
+  managerEmail: string | null;
+  campaignThumbnail: { create: { fileUrl: string } };
+  campaignImage?: {
+    createMany: {
+      data: { fileUrl: string }[];
+    };
+  };
+}
+
+export interface CreateVisitingCampaignInput {
+  brandId: number;
+  advertiserNo: number;
+  title: string;
+  reward: number;
+  channelConditionId: number;
+  postingGuide: string | null;
+  caution: string | null;
+  type: number;
+  recruitment: number;
+  recruitmentStartsDate: Date;
+  recruitmentEndsDate: Date;
+  selectionEndsDate: Date;
+  submitStartsDate: Date;
+  submitEndsDate: Date;
+  hashtag: string;
+  company: string;
+  managerName: string;
+  managerTel: string;
+  managerEmail: string | null;
+  campaignVisitingInfo: {
+    create: {
+      note: string;
+      visitingAddr: string;
+      visitingTime: string;
+      visitingEndsDate: Date;
+      servicePrice: number;
+      info: string;
+    };
+  };
+  campaignThumbnail: { create: { fileUrl: string } };
+  campaignImage?: {
+    createMany: {
+      data: { fileUrl: string }[];
+    };
+  };
+  campaignOption?: {
+    createMany: {
+      data: { name: string; value: string }[];
+    };
+  };
+}
