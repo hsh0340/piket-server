bee6f1341243a91e5ddf300b3a3b4db0bc884b6e
hsh0340 <hsh0340@naver.com>
2023-10-31 17:14:51 +0900

    Refactor: tokenPayload 타입지정

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -17,17 +17,16 @@ export class AuthService {
 
   async issueRefreshToken(userNo: number): Promise<string> {
     const payload = { userNo };
-    const refreshToken = await this.jwtService.signAsync(payload, {
+    return await this.jwtService.signAsync(payload, {
       secret: this.configService.get<string>('JWT_SECRET_KEY'),
       expiresIn: this.configService.get<string>('REFRESH_TOKEN_EXPIRATION'),
     });
-    return refreshToken;
   }
 
   /**
    * 토큰 유효성 검사 메서드
    */
-  async validateToken(tokenPayload): Promise<boolean> {
+  async validateToken(tokenPayload: { exp: number }): Promise<boolean> {
     const { exp } = tokenPayload;
     // 토큰 만료되면 false
     return exp >= Date.now() / 1000;
